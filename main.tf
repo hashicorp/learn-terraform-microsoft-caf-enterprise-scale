1f7ad06838eeba3b4ff81a9ec25935cf907bb1e1
George <dutza.geoorge@gmail.com>
2022-09-22 10:12:13 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,4 +21,13 @@ module "enterprise_scale" {
   deploy_corp_landing_zones   = local.deploy_corp_landing_zones
   deploy_online_landing_zones = local.deploy_online_landing_zones
   deploy_sap_landing_zones    = local.deploy_sap_landing_zones
+  
+  # Configuration settings for management resources.
+  # These are used to ensure Azure Policy is correctly configured with the same 
+  # settings as the resources deployed by module.enterprise_scale_management.
+  # Please refer to file: settings.management.tf
+  deploy_management_resources    = true
+  configure_management_resources = local.configure_management_resources
+  subscription_id_management     = data.azurerm_client_config.management.subscription_id
+  
 }
