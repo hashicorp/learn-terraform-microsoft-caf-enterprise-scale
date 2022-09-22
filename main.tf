445d1367393146cbbb792cc110cc2c84edea52be
George <dutza.geoorge@gmail.com>
2022-09-22 09:38:51 +0300

    Update main.tf
    adding custom landing zone LearnTerraform

--- main.tf
+++ main.tf
@@ -1,24 +1,39 @@
 # Declare the Terraform Module for Cloud Adoption Framework
 # Enterprise-scale and provide a base configuration.
 module "enterprise_scale" {
-  source  = "Azure/caf-enterprise-scale/azurerm"
-  version = "~> 1.0.0"
+          source  = "Azure/caf-enterprise-scale/azurerm"
+          version = "~> 1.0.0"
 
-  providers = {
-    azurerm              = azurerm
-    azurerm.management   = azurerm.management
-    azurerm.connectivity = azurerm.connectivity
-  }
+                    providers = {
+                    azurerm              = azurerm
+                    azurerm.management   = azurerm.management
+                    azurerm.connectivity = azurerm.connectivity
+                    }
 
-  # Base module configuration settings.
-  root_parent_id   = data.azurerm_client_config.current.tenant_id
-  root_id          = local.root_id
-  root_name        = local.root_name
-  default_location = local.default_location
+          # Base module configuration settings.
+          root_parent_id   = data.azurerm_client_config.current.tenant_id
+          root_id          = local.root_id
+          root_name        = local.root_name
+          default_location = local.default_location
 
-  # Control deployment of the core landing zone hierachy.
-  deploy_core_landing_zones   = true
-  deploy_corp_landing_zones   = local.deploy_corp_landing_zones
-  deploy_online_landing_zones = local.deploy_online_landing_zones
-  deploy_sap_landing_zones    = local.deploy_sap_landing_zones
+          # Control deployment of the core landing zone hierachy.
+          deploy_core_landing_zones   = true
+          deploy_corp_landing_zones   = local.deploy_corp_landing_zones
+          deploy_online_landing_zones = local.deploy_online_landing_zones
+          deploy_sap_landing_zones    = local.deploy_sap_landing_zones
+          
+  # Define an additional "LearnTerraform" Management Group.
+  custom_landing_zones = {
+          "${local.root_id}-learn-tf" = {
+                  display_name               = "LearnTerraform"
+                  parent_management_group_id = "${local.root_id}-landing-zones"
+                  subscription_ids           = []
+                          archetype_config = {
+                          archetype_id   = "default_empty"
+                          parameters     = {}
+                          access_control = {}
+                                            }
+                                         }
+                        }
+
 }
