c036b47778a5f1636360d5890596e5c9ee461c85
George <dutza.geoorge@gmail.com>
2022-09-22 09:30:33 +0300

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 2.77.0"
-       configuration_aliases = [
+      configuration_aliases = [
         azurerm.management,
         azurerm.connectivity,
       ]
